54ef8e5c7792d9c5e4ba3204288a5299e0ca8c80
Micherlane Silva <micherlanerod@gmail.com>
2023-08-12 15:51:36 -0300

    Servindo os arquivo estáticos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FixedIncomeController } from './controllers/investiment/FixedIncomeController';
 import { FixedIncomeService } from './services/investiment/FixedIncomeService';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController, FixedIncomeController],
   providers: [AppService, FixedIncomeService],
 })
